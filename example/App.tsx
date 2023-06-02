46a284b9f8950fa2d8c85936636b86f3fcff3ef4
Canon <cm090.tech@gmail.com>
2023-06-02 15:29:52 -0400

    Initial version of Selector component

---
+++ example/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import Selector, { Data } from '../index';
+
+const data: Data[] = [
+  { label: 'Item 1' },
+  { label: 'Item 2' },
+  { label: 'Item 3', priority: true },
+  { label: 'Item 4' },
+  { label: 'Item 5' },
+  { label: 'Item 6' },
+  { label: 'Item 7', priority: true },
+  { label: 'Item 8' },
+];
+
+function App(): JSX.Element {
+  const [item, setItem] = React.useState<string | JSX.Element>('');
+  const onDataSelect = (e: Data) => setItem(e.label);
+
+  return (
+    <View>
+      <Selector
+        data={data}
+        onSelect={onDataSelect}
+      />
+      <Text>Selected: {item || 'None'}</Text>
+    </View>
+  );
+}
+
+export default App;
