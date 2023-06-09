e369fd3aaa237c0207ec265226dda2994aa0d873
Canon <cm090.tech@gmail.com>
2023-06-09 10:11:44 -0400

    Fix type imports

--- example/App.tsx
+++ example/App.tsx
@@ -6,7 +6,7 @@ import {
   Text,
   View,
 } from 'react-native';
-import Selector, { Data } from '../index';
+import Selector, { type Data } from '../index';
 
 const data: Data[] = [
   { label: 'Item 1' },
