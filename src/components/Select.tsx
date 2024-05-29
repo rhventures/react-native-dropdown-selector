a9e85794a962cd1a96db4e1bd1f9853f6dcc93af
Drake Ma <mad1@rose-hulman.edu>
2024-05-29 16:08:17 -0400

    Removed unused imports

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,6 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View, useColorScheme } from 'react-native';
-import dropdownArrow from '../assets/down.png';
+import { StyleSheet, Text, TouchableOpacity, View, useColorScheme } from 'react-native';
 import styles from '../styles';
 import type { Data, SelectorProperties } from '../types';
 import SelectionList from './SelectionList';
