c31db1e77f9e27c769c37214ae9c2c5091984667
Drake Ma <mad1@rose-hulman.edu>
2024-06-03 08:07:21 -0400

    Changed overflowNotif to boolean instead of number

--- src/components/MultiSelect.tsx
+++ src/components/MultiSelect.tsx
@@ -48,7 +48,7 @@ const MultiSelect = (props: MultiSelectProperties): JSX.Element => {
         onPress={clickSelector}
         ref={ref}
         onLayout={(e: LayoutChangeEvent) => {
-          setOverflowNotif(overflowNotif ? 0 : 1);
+          setOverflowNotif(!overflowNotif);
         }}
       >
         {selected.length
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -72,7 +72,7 @@ const Select = (props: SelectorProperties): JSX.Element => {
         display={listDisplay}
         setDisplay={setListDisplay}
         selectorRef={ref}
-        overflowNotif={0}
+        overflowNotif={true}
       />
     </View>
   );
