d5b4ef09db0ea92102e286dc5e1ea191fa3a24e6
Drake Ma <mad1@rose-hulman.edu>
2024-06-03 08:07:21 -0400

    Changed overflowNotif to boolean instead of number

--- src/types.d.ts
+++ src/types.d.ts
@@ -24,7 +24,7 @@ export interface ListProperties {
   display: boolean;
   setDisplay: React.Dispatch<React.SetStateAction<boolean>>;
   selectorRef: React.RefObject<TouchableOpacity>;
-  overflowNotif: number,
+  overflowNotif: boolean,
 }
 
 export interface MultiSelectProperties {
