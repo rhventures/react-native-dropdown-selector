98ce6941d7610ed6b2ea88f4bb939f4aac5f3037
Drake Ma <mad1@rose-hulman.edu>
2024-06-04 09:03:20 -0400

    Possible replacement for SelectionList

---
+++ src/components/SelectionListNew.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { useColorScheme } from 'react-native'
+import { View } from 'react-native';
+import styles from '../styles';
+import type { Data, ListProperties } from '../types';
+
+const SelectionListNew = (props: ListProperties): JSX.Element => {
+  return (<View>
+
+  </View>);
+};
+
+export default SelectionListNew;
