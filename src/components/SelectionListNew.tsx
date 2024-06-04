9745e38f31894dbfbb733386033ff2b050348124
Drake Ma <mad1@rose-hulman.edu>
2024-06-04 09:20:48 -0400

    Added essential values for a SelectionList

--- src/components/SelectionListNew.tsx
+++ src/components/SelectionListNew.tsx
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
-import { useColorScheme } from 'react-native'
+import { Dimensions, useColorScheme } from 'react-native'
 import { View } from 'react-native';
 import styles from '../styles';
 import type { Data, ListProperties } from '../types';
 
 const SelectionListNew = (props: ListProperties): JSX.Element => {
-  return (<View>
+  const [listHeight, setListHeight] : [
+      number,
+      React.Dispatch<React.SetStateAction<number>>
+    ] = useState<number>(0),
+    windowHeight: number = Dimensions.get('window').height,
+    windowWidth: number = Dimensions.get('window').width,
+    style = useColorScheme() === 'dark' ? styles[1] : styles[0];
 
+  return (<View>
+    
   </View>);
 };
 
