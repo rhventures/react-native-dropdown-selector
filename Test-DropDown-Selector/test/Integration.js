c878c53d71e53f018fe1ce40adb0e239e421af90
rhit-jungh <jungh@rose-hulman.edu>
2024-06-05 14:15:17 -0400

    Integration test cases refactored

--- Test-DropDown-Selector/test/Integration.js
+++ Test-DropDown-Selector/test/Integration.js
@@ -1,14 +1,21 @@
 describe("Integration Test", () => {
 
-    context("Test Select with scrolling", ()=> {
+    const topSelector = '-android uiautomator:new UiSelector().text(\"ᨆ\").instance(0)';
+    const scrollCoordinates =[{'x': 642 , 'y1': 1050, 'y2': 900, 'y3': 560}]
 
-        beforeEach("When the first drop down selector is open", async () => {
-            const selector = await driver.$('-android uiautomator:new UiSelector().text(\"ᨆ\").instance(0)');
-            selector.click();
+    scrollAndSelectTest(topSelector, scrollCoordinates[0]);
 
-            await driver.pause(3000);
+    function scrollAndSelectTest(dropdown, scrollCoordinates){
+
+        context("Test Select with scrolling", ()=> {
+            
+            beforeEach("When the first drop down selector is open", async () => {
+                const selector = await driver.$(dropdown);
+                selector.click();
+                
+                await driver.pause(3000);
         })
-
+        
         it("should display item 3 is selected", async () => {
             const Item3 = await driver.$('accessibility id:Item 3')
             Item3.click();
@@ -19,7 +26,7 @@ describe("Integration Test", () => {
             console.log(selectorDisplay);
             await expect(selectorDisplay).toHaveText('Item 3')
         })
-
+        
         it("should display item 4 is selected", async () => {
             const Item4 = await driver.$('accessibility id:Item 4')
             Item4.click();
@@ -30,15 +37,15 @@ describe("Integration Test", () => {
             console.log(selectorDisplay);
             await expect(selectorDisplay).toHaveText('Item 4')
         })        
-
+        
         it("should display item 6 is selected", async () => {
             await driver.action('pointer', {parameters: {pointerType: 'touch'}})
-                    .move({duration : 0, x: 642 , y: 1050})
-                    .down({button: 0})
-                    .move({duration : 100, x: 642 , y: 790})
-                    .up({button: 0})
-                    .perform();
-
+            .move({duration : 0, x: scrollCoordinates['x'] , y: scrollCoordinates['y1']})
+            .down({button: 0})
+            .move({duration : 100, x: scrollCoordinates['x'] , y: scrollCoordinates['y2']})
+            .up({button: 0})
+            .perform();
+            
             await driver.action('pointer')
             const Item6 = await driver.$('accessibility id:Item 6')
             Item6.click();
@@ -49,15 +56,15 @@ describe("Integration Test", () => {
             console.log(selectorDisplay);
             await expect(selectorDisplay).toHaveText('Item 6')
         })
-
+        
         it("should display item abc is selected", async () => {
             await driver.action('pointer', {parameters: {pointerType: 'touch'}})
-                    .move({duration : 0, x: 642 , y: 1095})
-                    .down({button: 0})
-                    .move({duration : 100, x: 642 , y: 660})
-                    .up({button: 0})
-                    .perform();
-
+            .move({duration : 0, x: scrollCoordinates['x'] , y: scrollCoordinates['y1']})
+            .down({button: 0})
+            .move({duration : 100, x: scrollCoordinates['x'] , y: scrollCoordinates['y3']})
+            .up({button: 0})
+            .perform();
+            
             const ItemABC = await driver.$('accessibility id:abc')
             ItemABC.click();
             const selector = await driver.$('accessibility id:abc, ᨆ');
@@ -67,6 +74,7 @@ describe("Integration Test", () => {
             console.log(selectorDisplay);
             await expect(selectorDisplay).toHaveText('abc');
         })
-
-    })
+        
+        })
+    }
 })
