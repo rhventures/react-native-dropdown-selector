6b4d261d663436a938c3b637122a4a16a0f14921
rhit-jungh <jungh@rose-hulman.edu>
2024-06-11 08:30:02 -0400

    single select for top bar in iOS testing passes

--- Test-iOS/test/unit_SingleSelect.js
+++ Test-iOS/test/unit_SingleSelect.js
@@ -11,14 +11,68 @@ describe('selecting one element', () => {
         })
         it('should have item 3 selected', async() => {
             await driver.action('pointer', {parameters: {pointerType: 'touch'}})
-            .move({duration : 200, x: 164, y: 148})
+            .move({duration : 200, x: 190, y: 150})
             .down({button: 0})
             .up({button : 0})
             .perform();
             await driver.pause(1000);
 
             const dropDown = await driver.$('-ios class chain:**/XCUIElementTypeOther[`name == "Item 3 ᨆ"`][2]');
+            await dropDown.waitForExist(500)
+            expect (dropDown).toExist();
+        })
+
+        it('should have item 7 selected', async() => {
+            await driver.action('pointer', {parameters: {pointerType: 'touch'}})
+            .move({duration : 200, x: 190, y: 190})
+            .down({button: 0})
+            .up({button : 0})
+            .perform();
+            await driver.pause(1000);
+
+            const dropDown = await driver.$('-ios class chain:**/XCUIElementTypeOther[`name == "Item 7 ᨆ"`][2]');
+            await dropDown.waitForExist(500)
+            expect (dropDown).toExist();
+        })
+
+        it('should have item 4 selected', async() => {
+            await driver.action('pointer', {parameters: {pointerType: 'touch'}})
+            .move({duration : 200, x: 190, y: 310})
+            .down({button: 0})
+            .up({button : 0})
+            .perform();
+            await driver.pause(1000);
+
+            const dropDown = await driver.$('-ios class chain:**/XCUIElementTypeOther[`name == "Item 4 ᨆ"`][2]');
+            await dropDown.waitForExist(500)
+            expect (dropDown).toExist();
+        })
+
+        it('should have item 1 selected', async() => {
+            await driver.action('pointer', {parameters: {pointerType: 'touch'}})
+            .move({duration : 200, x: 190, y: 230})
+            .down({button: 0})
+            .up({button : 0})
+            .perform();
+            await driver.pause(1000);
+
+            const dropDown = await driver.$('-ios class chain:**/XCUIElementTypeOther[`name == "Item 1 ᨆ"`][2]');
+            await dropDown.waitForExist(500)
+            expect (dropDown).toExist();
+        })
+
+        it('should have item 2 selected', async() => {
+            await driver.action('pointer', {parameters: {pointerType: 'touch'}})
+            .move({duration : 200, x: 190, y: 270})
+            .down({button: 0})
+            .up({button : 0})
+            .perform();
+            await driver.pause(1000);
+
+            const dropDown = await driver.$('-ios class chain:**/XCUIElementTypeOther[`name == "Item 2 ᨆ"`][2]');
+            await dropDown.waitForExist(500)
             expect (dropDown).toExist();
         })
     })
+    
 })
