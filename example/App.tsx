53ccdaec9aebc5af6033cd9ceb0b1f7d25b0c754
Drake Ma <mad1@rose-hulman.edu>
2024-06-13 15:27:15 -0400

    limegreen to yellow

--- example/App.tsx
+++ example/App.tsx
@@ -178,7 +178,7 @@ function App(): JSX.Element {
             width: 300,
           }}
           boxTextStyle={{
-            color: 'limegreen',
+            color: 'yellow',
             fontFamily: 'courier new',
             fontSize: 24,
             fontStyle: 'italic',
@@ -219,7 +219,7 @@ function App(): JSX.Element {
             fontSize: 20,
           }}
           selectedItemStyle={{
-            backgroundColor: 'limegreen',
+            backgroundColor: 'yellow',
             borderTopColor: 'darkgreen',
             borderBottomColor: 'darkgreen',
             borderTopWidth: 2,
