c1984eef158478d74d01677148f4c171d385863f
Drake Ma <mad1@rose-hulman.edu>
2024-08-06 10:28:49 -0400

    Fix: Remove listX and listWidth

--- src/types.d.ts
+++ src/types.d.ts
@@ -30,8 +30,6 @@ export interface ListProperties {
   onSelect: ((e: Data) => void) | ((e: Data[]) => void);
   selected: Data[] | Data;
   clearSelected?: () => void;
-  listX: number;
-  listWidth: string | number;
   listHeight: number;
   display: boolean;
   hide: () => void;
